b0c3150afc0fbbd9fdce92b346b2ecd9941ca059
aconkli9 <aconkli9@ford.com>
2022-04-13 18:10:29 -0400

    Add dropdown

--- episode.js
+++ episode.js
@@ -30,6 +30,7 @@ fetchAndParse(`https://rickandmortyapi.com/api/episode/${episodeIdMap(currentSea
 function createEpisodeDiv(episode) {
     const div = document.querySelector("#episode-info")
     div.innerHTML = `
+        <h2>Season ${currentSeason}</h2>
         <h2>Episode ${currentEpisode} - ${episode.name} </h2>
         <img src="${episode.image.original}"/></img>
         <p class="episode-description">${episode.summary}</p>`
@@ -85,3 +86,80 @@ function fetchAndParse(url) {
 function redirect() {
     window.location.href = `404.html`
 }
+
+const form = document.querySelector("form")
+form.innerHTML = `
+    <label for="episode-dropdown"></label>
+    <select id="episode-dropdown" name="episode-dropdown">
+    <option value="select" disabled selected> Select an episode</option>
+        <optgroup label="Season 1"> 
+            <option value="1-1">Episode 1</option>
+            <option value="1-2">Episode 2</option>
+            <option value="1-3">Episode 3</option>
+            <option value="1-4">Episode 4</option>
+            <option value="1-5">Episode 5</option>
+            <option value="1-6">Episode 6</option>
+            <option value="1-7">Episode 7</option>
+            <option value="1-8">Episode 8</option>
+            <option value="1-9">Episode 9</option>
+            <option value="1-10">Episode 10</option>
+            <option value="1-11">Episode 11</option>
+        </optgroup>
+        <optgroup label="Season 2"> 
+            <option value="2-1">Episode 1</option>
+            <option value="2-2">Episode 2</option>
+            <option value="2-3">Episode 3</option>
+            <option value="2-4">Episode 4</option>
+            <option value="2-5">Episode 5</option>
+            <option value="2-6">Episode 6</option>
+            <option value="2-7">Episode 7</option>
+            <option value="2-8">Episode 8</option>
+            <option value="2-9">Episode 9</option>
+            <option value="2-10">Episode 10</option>
+        </optgroup>
+        <optgroup label="Season 3"> 
+            <option value="3-1">Episode 1</option>
+            <option value="3-2">Episode 2</option>
+            <option value="3-3">Episode 3</option>
+            <option value="3-4">Episode 4</option>
+            <option value="3-5">Episode 5</option>
+            <option value="3-6">Episode 6</option>
+            <option value="3-7">Episode 7</option>
+            <option value="3-8">Episode 8</option>
+            <option value="3-9">Episode 9</option>
+            <option value="3-10">Episode 10</option>
+        </optgroup>
+        <optgroup label="Season 4"> 
+            <option value="4-1">Episode 1</option>
+            <option value="4-2">Episode 2</option>
+            <option value="4-3">Episode 3</option>
+            <option value="4-4">Episode 4</option>
+            <option value="4-5">Episode 5</option>
+            <option value="4-6">Episode 6</option>
+            <option value="4-7">Episode 7</option>
+            <option value="4-8">Episode 8</option>
+            <option value="4-9">Episode 9</option>
+            <option value="4-10">Episode 10</option>
+        </optgroup>
+        <optgroup label="Season 5"> 
+            <option value="5-1">Episode 1</option>
+            <option value="5-2">Episode 2</option>
+            <option value="5-3">Episode 3</option>
+            <option value="5-4">Episode 4</option>
+            <option value="5-5">Episode 5</option>
+            <option value="5-6">Episode 6</option>
+            <option value="5-7">Episode 7</option>
+            <option value="5-8">Episode 8</option>
+            <option value="5-9">Episode 9</option>
+            <option value="5-10">Episode 10</option>
+        </optgroup>
+    </select>
+`
+
+form.addEventListener("change", (event) => {
+    event.preventDefault()
+    const selection = event.target.value
+    const season = selection.split('-')[0]
+    const episode = selection.split('-')[1]
+    window.location.href = `episode.html?season=${season}&episode=${episode}`
+})
